1da707d7e44a4d0732ea0f5192a3ecc69088cdab
kamiliahs <denied911025@gmail.com>
2026-04-18 23:55:42 -0300

    Mejoras en la interfaz

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Proporciona funcionalidad offline y caché de recursos
  */
 
-const CACHE_NAME = 'pos-minimalist-v1.0.20';
+const CACHE_NAME = 'pos-minimalist-v1.0.21';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
