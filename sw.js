032929a9d6cbd3e9edeacd134d3195a038d5a837
Daniel Ernesto Fernandez Gonzalez <deneid911025@gmail.com>
2026-05-12 21:33:35 -0300

    Adding adds

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Proporciona funcionalidad offline y caché de recursos
  */
 
-const CACHE_NAME = 'pos-minimalist-v1.0.23';
+const CACHE_NAME = 'pos-minimalist-v1.0.24';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
